9433b662e184cdde383de412ad417861b93a2d96
mezbaaaa <tahsinul101@gmail.com>
2025-07-13 03:13:05 +0600

    login page added

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { Link } from 'react-router';
 
 const Login = () => {
-    return (
+  return (
+    <div className="w-full max-w-md mx-auto text-left">
+      <h1 className="text-4xl font-extrabold mb-2">Welcome Back</h1>
+      <p className="mb-6 text-gray-600">Login with Profast</p>
+
+      <div className="flex flex-col gap-4">
         <div>
-            hhhhhhhhhhhhhhhiiiiiiiiiiiiiiii
+          <label className="block mb-1 text-sm font-medium text-gray-700">Email</label>
+          <input
+            type="email"
+            name='email'
+            placeholder="Email"
+            className="w-full px-4 py-3 border rounded-md focus:outline-none "
+          />
         </div>
-    );
+
+        <div>
+          <label className="block mb-1 text-sm font-medium text-gray-700">Password</label>
+          <input
+            type="password"
+            name='password'
+            placeholder="Password"
+            className="w-full px-4 py-3 border rounded-md focus:outline-none "
+          />
+        </div>
+
+        <div className="text-left">
+          <a href="#" className="text-sm text-gray-500 hover:underline">Forgot Password?</a>
+        </div>
+
+        <button className="w-full py-3 bg-[#CAEB66] cursor-pointer text-black font-semibold rounded-md">
+          Login
+        </button>
+
+        <p className="text-sm text-gray-500 text-left">
+          Don’t have any account? <Link className="text-lime-600 font-medium hover:underline">Register</Link>
+        </p>
+
+        <div className="flex items-center gap-2 mt-2">
+          <div className="flex-grow h-px bg-gray-300"></div>
+          <span className="text-sm text-gray-400">Or</span>
+          <div className="flex-grow h-px bg-gray-300"></div>
+        </div>
+
+        <button className="flex items-center justify-center gap-2 w-full py-3 bg-gray-100 border border-gray-300 rounded-md hover:bg-gray-200 cursor-pointer">
+          <FcGoogle size={20} />
+          <span className="text-base font-medium text-gray-700">Login with Google</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/layouts/AuthRoots.jsx
+++ src/layouts/AuthRoots.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import authImage from '../../src/assets/authImage.png';
-import { Outlet } from 'react-router';
+import { Link, Outlet } from 'react-router';
+import logo from '../../src/assets/logo.png'
 
 const AuthRoots = () => {
-  return (
-    <div className='relative'>
-        <div className='absolute'>
-            hello
+    return (
+        <div className='relative'>
+            <div className='absolute left-10 top-10'>
+                <Link to={'/'} className='flex items-end cursor-pointer'>
+                    <img src={logo} alt="logo Image" className='mb-2' />
+                    <p className='font-extrabold text-3xl -ml-4'>Profast</p>
+                </Link>
+            </div>
+            <div className="grid grid-cols-2 min-h-screen text-center">
+                <div className="col-span-1 flex items-center justify-center">
+                    <Outlet />
+                </div>
+                <div className="col-span-1 bg-gradient-to-r from-[#DEF29F] to-[#E8F6BD] flex items-center justify-center">
+                    <img src={authImage} alt="Auth" className="max-w-full h-auto" />
+                </div>
+            </div>
         </div>
-        <div className="grid grid-cols-2 min-h-screen text-center">
-      <div className="col-span-1 flex items-center justify-center">
-        <Outlet/>
-      </div>
-      <div className="col-span-1 bg-gradient-to-r from-[#DEF29F] to-[#E8F6BD] flex items-center justify-center">
-        <img src={authImage} alt="Auth" className="max-w-full h-auto" />
-      </div>
-    </div>
-    </div>
-  );
+    );
 };
 
 export default AuthRoots;
