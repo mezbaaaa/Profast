f3fe881259cb95d27571ec61eb92650ed25732f1
mezbaaaa <tahsinul101@gmail.com>
2025-07-14 14:17:34 +0600

    google login added

--- src/components/pages/auth/SoicalLogin.jsx
+++ src/components/pages/auth/SoicalLogin.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FcGoogle } from 'react-icons/fc';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router';
 
 const SoicalLogin = () => {
+    const { googleSignin } = useContext(AuthContext)
+    const navigate=useNavigate()
+
+    const handleGooleLogin=()=>{
+        googleSignin().then(result=>{
+            const user = result.user;
+            console.log(user)
+            if(user){
+                navigate('/')
+            }
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
+
     return (
         <div>
-            <button className="flex items-center justify-center gap-2 w-full py-3 bg-gray-100 border border-gray-300 rounded-md hover:bg-gray-200 cursor-pointer">
-          <FcGoogle size={20} />
-          <span className="text-base font-medium text-gray-700">Login with Google</span>
-        </button>
+            <button onClick={handleGooleLogin} className="flex items-center justify-center gap-2 w-full py-3 bg-gray-100 border border-gray-300 rounded-md hover:bg-gray-200 cursor-pointer">
+                <FcGoogle size={20} />
+                <span className="text-base font-medium text-gray-700">Login with Google</span>
+            </button>
         </div>
     );
 };
