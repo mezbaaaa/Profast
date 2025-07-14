8f330f5ab4147cb77265e8b96967f0d643973253
mezbaaaa <tahsinul101@gmail.com>
2025-07-14 14:07:08 +0600

    observer added to get current user

--- src/components/context/AuthProvider.jsx
+++ src/components/context/AuthProvider.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import { auth } from '../firebase/firebase.init';
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const provider = new GoogleAuthProvider();
 
-    const userInfo={
+    const googleSignin = () => {
+        return signInWithPopup(auth, provider)
+    }
 
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('current user', currentUser)
+            setUser(currentUser)
+        })
+        return unSubscribe
+    }, [])
+
+    const userInfo = {
+        user,
+        googleSignin
     }
     return (
         <AuthContext value={userInfo}>
